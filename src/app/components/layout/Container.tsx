24ccfd8218225c23f1252d377b8e9699038fd66d
Felipe Mateus <felipe_mateus08@hotmail.com>
2025-05-14 00:10:41 -0300

    feat(product-page): create page and initial components

---
+++ src/app/components/layout/Container.tsx
@@ -0,0 +1,14 @@
+import { cn } from '@/lib/utils/cn'
+
+type ContainerProps = {
+  className?: string
+  children?: React.ReactNode
+}
+
+export default function Container({ className, children }: ContainerProps) {
+  return (
+    <div className={cn('mx-auto h-full w-full max-w-6xl', className)}>
+      {children}
+    </div>
+  )
+}
