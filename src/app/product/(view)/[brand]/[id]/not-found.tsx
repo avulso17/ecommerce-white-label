a054793d3655a28a85627e35e9dba3e3636690f3
Felipe Mateus <felipe_mateus08@hotmail.com>
2025-05-20 16:41:34 -0300

    feat(product-page): add error handling page

--- src/app/product/(view)/[brand]/[id]/not-found.tsx
+++ src/app/product/(view)/[brand]/[id]/not-found.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className='flex h-full w-full flex-col items-center justify-center gap-6'>
-      <h1 className='text-lg font-medium'>Product not found :(</h1>
+      <h2 className='text-lg font-medium'>Product not found :(</h2>
       <Link href='/'>
         <Button>Go to Home</Button>
       </Link>
