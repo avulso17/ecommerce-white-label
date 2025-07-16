237595d514fbb8aea7ae45e8d708bb71e281e6e4
Felipe Mateus <felipe_mateus08@hotmail.com>
2025-07-16 00:05:46 -0300

    feat: create click test on navbar buttons

--- src/components/layout/navbar/Navbar.spec.tsx
+++ src/components/layout/navbar/Navbar.spec.tsx
@@ -10,4 +10,36 @@ describe('Navbar', () => {
     expect(screen.getByText('About')).toBeInTheDocument()
     expect(screen.getByText('Contact')).toBeInTheDocument()
   })
+
+  it('logo should redirect to homepage when click', () => {
+    render(<Navbar />)
+
+    const LogoEl = screen.getByRole('link', { name: /logo Ecommerce/i })
+    expect(LogoEl).toHaveAttribute('href', '/')
+  })
+
+  it('home link should redirect to homepage when click', () => {
+    render(<Navbar />)
+
+    const homeLink = screen.getByRole('link', { name: /home/i })
+    expect(homeLink).toHaveAttribute('href', '/')
+  })
+
+  it('cart button should redirect to cartpage when click', () => {
+    render(<Navbar />)
+
+    const cartBtn = screen
+      .getAllByRole('link')
+      .find((link) => link.getAttribute('href') === '/cart')
+    expect(cartBtn).toBeInTheDocument()
+  })
+
+  it('profile button should redirect to profilepage when click', () => {
+    render(<Navbar />)
+
+    const profileBtn = screen
+      .getAllByRole('link')
+      .find((link) => link.getAttribute('href') === '/profile')
+    expect(profileBtn).toBeInTheDocument()
+  })
 })
